bc243bd6c17054df654d6c80a8bc05cdc2facd12
ronniebm <ronnie.coding@gmail.com>
2020-11-09 10:10:36 -0500

    React 1st App, rendering a component in a DIV with DOM

---
+++ 03-counter-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const greeting = <h1>Hello world !</h1>;
+
+const divRoot = document.querySelector('#app');
+
+
+ReactDOM.render(greeting, divRoot); 
