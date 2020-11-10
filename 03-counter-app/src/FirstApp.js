5be601eeff21652055c43277fa5d24171f613921
ronniebm <ronnie.coding@gmail.com>
2020-11-10 09:40:07 -0500

    updating Readme Scope

--- 03-counter-app/src/FirstApp.js
+++ 03-counter-app/src/FirstApp.js
@@ -14,6 +14,7 @@
 //     now 'Functional Components' (FC), so class based components are not been used.
 
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 
 // according to mentioned before, this is our first FC (Functional Component):
 const FirstApp = () => {
@@ -21,6 +22,9 @@ const FirstApp = () => {
     return <h1>Ronnie, this is a Hello world component !</h1>;
 
 }
+
+
+// ***************************************************************************
 
 // creating a second component to learn how to return 2 tags, for this we must "wrap" them,
 // let's do that with a DIV parent tag.
@@ -39,6 +43,8 @@ export const SecondApp = () => {
 }
 
 
+// ***************************************************************************
+
 // this third component doesn't generate a wraping DIV, it will return the <h1> and <p> tags only !,
 // and are some cases when we're gonna need this, that it's ok too.
 export const ThirdApp = () => {
@@ -50,6 +56,8 @@ export const ThirdApp = () => {
     );
 }
 
+
+// ***************************************************************************
 
 // after learning about 'Fragments', i found a short way of do 'FRAGMENTS' but without need to import 'Fragment' module
 // from React modules, and it's just wrapping elements with empty opening and closing tags like this =>   <> </>.
@@ -63,6 +71,105 @@ export const FourthApp = () => {
     );
 }
 
+// ***************************************************************************
+
+// In App5 we're passing values trough props, without use of propTypes Module.
+// --- also, if the 'greeting' prop is not passed from the parent, then i will
+//     asign a Default Message, that is gonna be showed in this case.
+export const App5 = ( { greeting  = 'Default Message !' } ) => {
+
+    return (
+        <>
+            <h1> { greeting } </h1>
+            <p>My App#5.</p>
+        </>
+    );
+}
+
+
+// ***************************************************************************
+
+export const App6 = ( { greeting } ) => {
+
+    // In following block we can generate a personalized Error Message,
+    // to make the programmer understand why has failed ! and it's usefull !.
+    // But this way generates some extra memory compsumption and we must take
+    // this into account !!!
+    if ( !greeting ) {
+        /* throw new Error("It's neccesary a greeting in the given props !"); */
+    }
+
+    return (
+        <>
+            <h1> { greeting } </h1>
+            <p>My App#6.</p>
+        </>
+    );
+}
+
+
+// ***************************************************************************
+export const App7 = ( { greeting } ) => {
+
+    // In following block we can generate a personalized Error Message,
+    // to make the programmer understand why has failed ! and it's usefull !.
+    // But this way generates some extra memory compsumption and we must take
+    // this into account !!!
+    if ( !greeting ) {
+        /* throw new Error("It's neccesary a greeting in the given props !"); */
+    }
+
+    return (
+        <>
+            <h1> { greeting } </h1>
+            <p>My App#7.</p>
+        </>
+    );
+}
+
+// using propTypes we can advice to another programmer that
+// he/she must pass these declared props, and with exactly
+// those predefined types !.
+App7.propTypes = {
+    greeting: PropTypes.string.isRequired,
+    otherprop: PropTypes.number.isRequired
+}
+
+
+
+// ***************************************************************************
+export const App8 = ( { greeting, subtitle } ) => {
+
+    // In following block we can generate a personalized Error Message,
+    // to make the programmer understand why has failed ! and it's usefull !.
+    // But this way generates some extra memory compsumption and we must take
+    // this into account !!!
+    if ( !greeting ) {
+        /* throw new Error("It's neccesary a greeting in the given props !"); */
+    }
+
+    return (
+        <>
+            <h1> { greeting } </h1>
+            <p> { subtitle } </p>
+        </>
+    );
+}
+
+App8.propTypes = {
+    greeting: PropTypes.string.isRequired
+}
+
+App8.defaultProps = {
+    subtitle: "I'm a subtitle (default value)"
+}
+
+
+
+// ********************** EXPORT BY DEFAULT SECTION **************************
 
 // exporting by default this component.
 export default FirstApp;
+
+
+
--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -3,7 +3,8 @@
 // importing the 'FirstApp' component.
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FirstApp, { SecondApp, ThirdApp, FourthApp } from './FirstApp';
+import FirstApp, { SecondApp, ThirdApp, FourthApp, App5, App6, App7, App8} from './FirstApp';
+import CounterApp from './CounterApp';
 
 // applying some style to my first component :D
 // importing the index.css file.
@@ -14,4 +15,14 @@ import './index.css';
 const divRoot = document.querySelector('#app');
 
 // rendering the 'FirstApp' and 'SecondApp' component into the divRoot asigned object.
-ReactDOM.render([<FirstApp/>, <SecondApp/>, <ThirdApp/>, <FourthApp/>], divRoot);
+ReactDOM.render([
+    <FirstApp/>,
+    <SecondApp/>,
+    <ThirdApp/>,
+    <FourthApp/>,
+    <App5 greeting ='Hi, i am Trunks !'/>,
+    <App6/>,
+    <App7/>,
+    <App8 greeting ='Greetings from App8 !'/>,
+    <CounterApp value = {12} />],
+divRoot);
